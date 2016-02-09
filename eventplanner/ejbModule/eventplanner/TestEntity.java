4121742c51b1ef0331e6f20963c084d59f196c70
Kristoffer <brnnmrk@Kristoffer-Brannmarks-MacBook-Pro.local>
2016-02-09 11:15:03 +0100

    Delete

--- eventplanner/ejbModule/eventplanner/TestEntity.java
+++
@@ -1,8 +0,0 @@
-package eventplanner;
-
-public class TestEntity {
-
-	//DETTA �R ETT TEST, MODAFOUKA
-	
-	//Jasså det säger du!
-}
--- eventplanner/ejbModule/eventplanner/testEJBmoduleDELETE.java
+++
@@ -1,5 +0,0 @@
-package eventplanner;
-
-public class testEJBmoduleDELETE {
-
-}
